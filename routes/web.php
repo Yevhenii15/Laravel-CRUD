656ff2d7c098df295c22c2be717776da98ac9453
Yevhenii <yevmot01@easv365.dk>
2025-02-04 14:33:24 +0100

    added colection cats

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
-
+use App\Http\Controllers\CatController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::resource('cats', CatController::class);
