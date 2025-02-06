34e0d8737270b4d88556fa0f1167f74da787e319
Yevhenii <yevmot01@easv365.dk>
2025-02-06 09:22:24 +0100

    new

--- database/migrations/2025_02_04_131658_create_cats_table.php
+++ database/migrations/2025_02_04_131658_create_cats_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('breed');
+            $table->integer('age');
             $table->timestamps();
         });
     }
