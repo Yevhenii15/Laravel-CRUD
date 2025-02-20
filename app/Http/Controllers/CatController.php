0b6b021e52287c574476b7590384cca07cba89d7
Yevhenii <yevmot01@easv365.dk>
2025-02-20 11:08:02 +0100

    added auth

--- app/Http/Controllers/CatController.php
+++ app/Http/Controllers/CatController.php
@@ -10,6 +10,10 @@ class CatController extends Controller
     /**
      * Display a listing of the resource.
      */
+    public function __construct()
+    {
+       $this->middleware('auth')->except(['index', 'show']);
+    }
     public function index()
     {
         $cats = Cat::latest()->paginate(5);
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,8 @@
 use App\Http\Controllers\CatController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 Route::resource('cats', CatController::class);
+Auth::routes();
+Route::get('/', [CatController::class, 'index']);
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
